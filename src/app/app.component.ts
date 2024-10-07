b478bad7edbd92bd249aecbabe376e80900cfbbc
odevjava <kevinandersonpaulino@gmail.com>
2024-10-07 16:43:50 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button, ButtonModule} from "primeng/button";
+import {FaIconComponent, FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./font-awesome-icons";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-clone-front';
+export class AppComponent implements OnInit {
+  faIconLibrary : FaIconLibrary = inject(FaIconLibrary)
+
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void{
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
+
 }
